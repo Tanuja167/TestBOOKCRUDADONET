2a2bde67bb290048cf001d6aee34781e88590551
Tanuja167 <104304824+Tanuja167@users.noreply.github.com>
2024-06-04 23:56:40 +0530

    Add project files.

---
+++ TestBOOK/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace TestBOOK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblauthor = new System.Windows.Forms.Label();
+            this.lblprice = new System.Windows.Forms.Label();
+            this.lblname = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtprice = new System.Windows.Forms.TextBox();
+            this.txtauthor = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnshowall = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(79, 63);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(18, 16);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // lblauthor
+            // 
+            this.lblauthor.AutoSize = true;
+            this.lblauthor.Location = new System.Drawing.Point(79, 236);
+            this.lblauthor.Name = "lblauthor";
+            this.lblauthor.Size = new System.Drawing.Size(45, 16);
+            this.lblauthor.TabIndex = 1;
+            this.lblauthor.Text = "Author";
+            // 
+            // lblprice
+            // 
+            this.lblprice.AutoSize = true;
+            this.lblprice.Location = new System.Drawing.Point(79, 171);
+            this.lblprice.Name = "lblprice";
+            this.lblprice.Size = new System.Drawing.Size(38, 16);
+            this.lblprice.TabIndex = 2;
+            this.lblprice.Text = "Price";
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(79, 112);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(44, 16);
+            this.lblname.TabIndex = 3;
+            this.lblname.Text = "Name";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(236, 60);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 22);
+            this.txtid.TabIndex = 4;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(236, 109);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 22);
+            this.txtname.TabIndex = 5;
+            // 
+            // txtprice
+            // 
+            this.txtprice.Location = new System.Drawing.Point(236, 168);
+            this.txtprice.Name = "txtprice";
+            this.txtprice.Size = new System.Drawing.Size(100, 22);
+            this.txtprice.TabIndex = 6;
+            // 
+            // txtauthor
+            // 
+            this.txtauthor.Location = new System.Drawing.Point(236, 230);
+            this.txtauthor.Name = "txtauthor";
+            this.txtauthor.Size = new System.Drawing.Size(100, 22);
+            this.txtauthor.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(425, 56);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(425, 229);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 9;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(425, 171);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 10;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(425, 112);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 11;
+            this.btnsave.Text = "save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnshowall
+            // 
+            this.btnshowall.Location = new System.Drawing.Point(830, 304);
+            this.btnshowall.Name = "btnshowall";
+            this.btnshowall.Size = new System.Drawing.Size(75, 23);
+            this.btnshowall.TabIndex = 12;
+            this.btnshowall.Text = "Show All";
+            this.btnshowall.UseVisualStyleBackColor = true;
+            this.btnshowall.Click += new System.EventHandler(this.btnshowall_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(629, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(471, 215);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 507);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnshowall);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtauthor);
+            this.Controls.Add(this.txtprice);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.lblprice);
+            this.Controls.Add(this.lblauthor);
+            this.Controls.Add(this.lblId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblauthor;
+        private System.Windows.Forms.Label lblprice;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtprice;
+        private System.Windows.Forms.TextBox txtauthor;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnshowall;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ TestBOOK/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Data;
+
+
+
+namespace TestBOOK
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader da;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString);
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string qry = "insert into BookTest values(@bname, @price, @author)";
+                cmd = new SqlCommand(qry, con);
+
+                cmd.Parameters.AddWithValue("@bname", txtname.Text);
+                cmd.Parameters.AddWithValue("@price", Convert.ToInt32(txtprice.Text));
+                cmd.Parameters.AddWithValue("@author", txtauthor.Text);
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if(result >= 1)
+                {
+                    MessageBox.Show("Record Inserted");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            GetAllBooks();
+            Clear();
+            
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select *from BookTest where bid= @id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtid.Text));
+                con.Open();
+                da = cmd.ExecuteReader();
+                if(da.HasRows)
+                {
+                    if(da.Read())
+                    {
+                        txtname.Text = da["bname"].ToString();
+                        txtprice.Text = da["price"].ToString();
+                        txtauthor.Text = da["author"].ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("record not found");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "update BookTest set price = @price where bid = @id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtid.Text));
+                cmd.Parameters.AddWithValue("@price", Convert.ToInt32(txtprice.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if(result >= 1)
+                {
+                    MessageBox.Show("Record updated");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            GetAllBooks();
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "delete from BookTest where bid = @id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtid.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result >= 1)
+                {
+                    MessageBox.Show("record deleted");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            GetAllBooks();
+            Clear();
+        }
+        private void GetAllBooks()
+        {
+            string qry = "select *from BookTest";
+            cmd = new SqlCommand(qry, con);
+            con.Open();
+            da = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(da);
+            dataGridView1.DataSource = table;
+            con.Close();
+
+
+        }
+        private void Clear()
+        {
+            txtid.Clear();
+            txtname.Clear();
+            txtauthor.Clear();
+            txtprice.Clear();
+
+        }
+
+        private void btnshowall_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
